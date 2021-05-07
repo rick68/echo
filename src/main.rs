d64a8a37524d8626828e885cd0d9afa0196f3560
Rick Yang <rick68@gmail.com>
2021-05-07 16:09:07 +0800

    improve the readability of code

--- src/main.rs
+++ src/main.rs
@@ -20,56 +20,52 @@ fn main() -> io::Result<()> {
         .filter(None, LevelFilter::Info)
         .init();
 
-    match TcpListener::bind((Ipv4Addr::UNSPECIFIED, 7)) {
-        Ok(listener) => {
-            let mut iter = listener.incoming();
-            loop {
-                if CONNECTS.load(Ordering::Relaxed) < CONNECT_LIMIT {
-                    CONNECTS.fetch_add(1, Ordering::Relaxed);
-                    if let Some(Ok(mut stream)) = iter.next() {
-                        let remote = stream.peer_addr()?;
-                        info!("{} Establish a connection", remote);
+    let listener = TcpListener::bind((Ipv4Addr::UNSPECIFIED, 7))?;
+    let mut iter = listener.incoming();
 
-                        spawn(move || -> io::Result<()> {
-                            let mut buf = [0; BUFFER_SIZE];
+    loop {
+        if CONNECTS.load(Ordering::Relaxed) < CONNECT_LIMIT {
+            CONNECTS.fetch_add(1, Ordering::Relaxed);
+            if let Some(Ok(mut stream)) = iter.next() {
+                let remote = stream.peer_addr()?;
+                info!("{} Establish a connection", remote);
 
-                            stream
-                                .set_read_timeout(Some(Duration::from_secs(CONNCETION_TIMEOUT)))?;
+                spawn(move || -> io::Result<()> {
+                    let mut buf = [0; BUFFER_SIZE];
 
-                            loop {
-                                match stream.read(&mut buf) {
-                                    Ok(0) => break,
-                                    Ok(n) => match stream.write(&buf[0..n]) {
-                                        Ok(0) => break,
-                                        Ok(_) => info!(
-                                            "{} {}",
-                                            remote,
-                                            String::from_utf8_lossy(&buf[0..n])
-                                                .trim_end_matches(['\r', '\n'].as_ref())
-                                        ),
-                                        Err(_) => break,
-                                    },
-                                    Err(e) => {
-                                        if e.kind() == ErrorKind::WouldBlock {
-                                            warn!("{} Connection timeout", remote);
-                                        }
-                                        break;
-                                    }
+                    stream.set_read_timeout(Some(Duration::from_secs(CONNCETION_TIMEOUT)))?;
+
+                    loop {
+                        match stream.read(&mut buf) {
+                            Ok(0) => break,
+                            Ok(n) => match stream.write(&buf[0..n]) {
+                                Ok(0) => break,
+                                Ok(_) => info!(
+                                    "{} {}",
+                                    remote,
+                                    String::from_utf8_lossy(&buf[0..n])
+                                        .trim_end_matches(['\r', '\n'].as_ref())
+                                ),
+                                Err(_) => break,
+                            },
+                            Err(e) => {
+                                if e.kind() == ErrorKind::WouldBlock {
+                                    warn!("{} Connection timeout", remote);
                                 }
+                                break;
                             }
-
-                            CONNECTS.fetch_sub(1, Ordering::Relaxed);
-                            info!("{} Disconnected", remote);
-
-                            Ok(())
-                        });
+                        }
                     }
-                } else {
-                    sleep(Duration::from_secs(1));
-                }
+
+                    CONNECTS.fetch_sub(1, Ordering::Relaxed);
+                    info!("{} Disconnected", remote);
+
+                    Ok(())
+                });
             }
+        } else {
+            sleep(Duration::from_secs(1));
         }
-        Err(e) => eprintln!("{}", e),
     }
 
     Ok(())
